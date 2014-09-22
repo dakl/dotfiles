1c783d79d06a768f55e2e24a213813ea43a3a9af
Daniel Klevebring <daniel.klevebring@gmail.com>
2014-09-22 15:41:12 +0200

    added init script

---
+++ init.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# create dirs
+mkdir -p $HOME/repos
+mkdir -p $HOME/bin
+
+cd $HOME
+
+# pyenv 
+git clone git://github.com/yyuu/pyenv.git ~/.pyenv
+
+## cpanminus
+curl https://raw.githubusercontent.com/miyagawa/cpanminus/master/cpanm | perl - -l ~/perl5 App::cpanminus local::lib
+eval `perl -I ~/perl5/lib/perl5 -Mlocal::lib`
+
+# dotfiles
+cd $HOME
+git clone https://github.com/dakl/dotfiles.git
+cd dotfiles
+source bootstrap.sh --force
+
+# python 2.7.8
+pyenv install 2.7.8
+
+## verticalize
+git clone https://github.com/lindenb/verticalize.git $HOME/repos/verticalize
+cd $HOME/repos/verticalize
+make
+cd $HOME/bin
+ln -s $HOME/repos/verticalize/verticalize
+
