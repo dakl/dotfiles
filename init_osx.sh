8d1cd64a7c60fd301545afa489bcebfedd74cc2a
Daniel Klevebring <daniel.klevebring@gmail.com>
2017-01-09 13:54:54 +0100

    commented out several brews

--- init_osx.sh
+++ init_osx.sh
@@ -19,7 +19,7 @@ if [[ "$(type -P brew)" ]]; then
   brew update
 
   # Install Homebrew recipes.
-  recipes=(git tree sl lesspipe htop-osx stress maven graphviz Caskroom/cask/brew-cask)
+  recipes=(git tree sl lesspipe htop-osx stress graphviz Caskroom/cask/brew-cask)
   for item in ${recipes[@]}; do
       brew install $item
   done
@@ -30,19 +30,19 @@ if [[ "$(type -P brew)" ]]; then
   fi
   
   #brew casks
-  brew cask install Caskroom/cask/macdown
-  brew cask install Caskroom/versions/java7
+  #brew cask install Caskroom/cask/macdown
+  #brew cask install Caskroom/versions/java7
   brew cask install Caskroom/cask/pycharm-ce
   #brew install github-release
-  brew install Caskroom/cask/r
-  brew install Caskroom/cask/rstudio
+  #brew install Caskroom/cask/r
+  #brew install Caskroom/cask/rstudio
   #brew install Caskroom/cask/virtualbox
-  brew install homebrew/versions/scala210
+  #brew install homebrew/versions/scala210
   #brew install Caskroom/cask/chefdk
   #brew install Caskroom/cask/puppet
   brew install Caskroom/cask/keepingyouawake
-  brew install Caskroom/cask/virtualbox
-  brew install Caskroom/cask/vagrant
+  #brew install Caskroom/cask/virtualbox
+  #brew install Caskroom/cask/vagrant
   brew install Caskroom/cask/sourcetree
 
 fi
