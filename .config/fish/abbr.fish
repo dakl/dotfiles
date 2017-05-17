434008eb831f749095501b9ca5f4c4d8e0e7869e
Daniel Klevebring <daniel.klevebring@fyndiq.com>
2017-05-17 08:28:02 +0200

    add abbr e

--- .config/fish/abbr.fish
+++ .config/fish/abbr.fish
@@ -1,6 +1,7 @@
 abbr l 'ls -laoh'
 abbr dc docker-compose
 abbr vim nvim
+abbr e nvim
 abbr venv 'source .venv/bin/activate.fish'
 abbr dclean 'docker ps --filter "status=exited" -q|xargs docker rm'
 abbr tc 'tee|pbcopy'
