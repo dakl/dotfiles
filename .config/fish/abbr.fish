38574950ac74d43bdf7c7b1fa618cf92931eaa7b
Daniel Klevebring <daniel.klevebring@fyndiq.com>
2017-09-25 08:15:05 +0200

    add kubectl abbrs

--- .config/fish/abbr.fish
+++ .config/fish/abbr.fish
@@ -3,10 +3,13 @@ abbr dc docker-compose
 abbr vim nvim
 abbr e nvim
 abbr venv 'source .venv/bin/activate.fish'
-abbr dclean 'docker ps --filter "status=exited" -q|xargs docker rm'
-abbr tc 'tee|pbcopy'
 abbr fgr 'find .|grep'
 abbr hgr 'history|grep'
 abbr ga 'git add'
 abbr gc 'git commit -m '
 abbr gco 'git checkout'
+
+abbr kc 'kubectl'
+abbr kgp 'kubectl get pods'
+abbr kgs 'kubectl get svc'
+abbr kgd 'kubectl get deploy'
