ee7e5fe48aebe414e8f07bbb0c6fe711e5e5a5e3
Daniel Klevebring <daniel.klevebring@fyndiq.com>
2017-10-10 08:18:28 +0200

    add abbrs for switching k8s envs

--- .config/fish/abbr.fish
+++ .config/fish/abbr.fish
@@ -13,3 +13,6 @@ abbr kc 'kubectl'
 abbr kgp 'kubectl get pods'
 abbr kgs 'kubectl get svc'
 abbr kgd 'kubectl get deploy'
+
+abbr k8s_prod 'gcloud container clusters get-credentials prod-cluster --zone europe-west1-c --project fyndiq-prod'
+abbr k8s_dev 'gcloud container clusters get-credentials dev-cluster --zone europe-west1-b --project fyndiq-dev'
