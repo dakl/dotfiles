d9cefeff8c7e9600ccba69ffefe89fdd068ccada
Daniel Klevebring <daniel.klevebring@gmail.com>
2017-10-18 21:27:49 +0200

    first vscode settings file

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,18 +9,30 @@ function doIt() {
 		--exclude "init_osx.sh" --exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
 
 	mkdir -p "$HOME/Library/Application Support/Code/User/"
-	KEYBINDINGS_FILE="$HOME/Library/Application Support/Code/User/keybindings.json"
-	if [[ -e "$KEYBINDINGS_FILE" ]]; then
-	    if [[ -L "$KEYBINDINGS_FILE" ]]; then # in bash, -L means "is a symbolic link".
+	VSCODE_KEYBINDINGS_FILE="$HOME/Library/Application Support/Code/User/keybindings.json"
+	VSCODE_SETTINGS_FILE="$HOME/Library/Application Support/Code/User/settings.json"
+	if [[ -e "$VSCODE_KEYBINDINGS_FILE" ]]; then
+	    if [[ -L "$VSCODE_KEYBINDINGS_FILE" ]]; then # in bash, -L means "is a symbolic link".
 			echo "Symlink for vscode keybindings already in place, skipping symlinking"
 		else
 		    echo "WARNING: vscode keybindings already exist, can't symlink."
 		fi
 	else
 	    echo "Symlinking vscode keybindings"
-		ln -s "$HOME/.vscode.keybindings.json" "$KEYBINDINGS_FILE"
+		ln -s "$HOME/.vscode.keybindings.json" "$VSCODE_KEYBINDINGS_FILE"
 	fi	
-	source ~/.bash_profile
+
+	if [[ -e "$VSCODE_SETTINGS_FILE" ]]; then
+	    if [[ -L "$VSCODE_SETTINGS_FILE" ]]; then # in bash, -L means "is a symbolic link".
+			echo "Symlink for vscode settings already in place, skipping symlinking"
+		else
+		    echo "WARNING: vscode settings already exist, can't symlink."
+		fi
+	else
+	    echo "Symlinking vscode settings"
+		ln -s "$HOME/.vscode.settings.json" "$VSCODE_SETTINGS_FILE"
+	fi	
+
 	echo "Done"
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
