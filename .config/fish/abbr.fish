4a5727f85df93cd95f7d6a5246db9926f3f74a4b
Daniel Klevebring <daniel.klevebring@gmail.com>
2017-12-18 08:35:55 +0100

    remove unused abbrs

--- .config/fish/abbr.fish
+++ .config/fish/abbr.fish
@@ -1,7 +1,5 @@
 abbr l 'ls -laoh'
 abbr dc docker-compose
-abbr vim nvim
-abbr e nvim
 abbr fgr 'find .|grep'
 abbr hgr 'history|grep'
 abbr ga 'git add'
@@ -9,9 +7,3 @@ abbr gc 'git commit -m '
 abbr gco 'git checkout'
 
 abbr kc 'kubectl'
-abbr kgp 'kubectl get pods'
-abbr kgs 'kubectl get svc'
-abbr kgd 'kubectl get deploy'
-
-abbr k8s_prod 'gcloud container clusters get-credentials prod-cluster --zone europe-west1-c --project fyndiq-prod'
-abbr k8s_dev 'gcloud container clusters get-credentials dev-cluster --zone europe-west1-b --project fyndiq-dev'
