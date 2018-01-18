e5a20ef0009356d56119016b94372a3f4c338ad0
Daniel Klevebring <daniel.klevebring@gmail.com>
2018-01-18 14:00:30 +0100

    make venv use commands

--- .config/fish/functions/venv.fish
+++ .config/fish/functions/venv.fish
@@ -1,11 +1,44 @@
-function venv --description 'create or activate a virtualenv'
+function venv --argument-names cmd VENV_NAME --description 'list (ls), create (mk) or activate (a) a virtualenv'
     set VENV_BASE $HOME/.virtualenvs
-    if [ $argv = "ls" ]
+
+    if [ -z $cmd ]
+        set cmd 'unset'
+    end
+
+    if [ -z $VENV_NAME ]
+        set VENV_NAME (basename (pwd))
+    end
+    
+    ## List availavle virtualenvs
+    if [ $cmd = "ls" ]
+        # list available virtualenvs
         ls $VENV_BASE
-    else if test -e $VENV_BASE/$argv
-        source $VENV_BASE/$argv/bin/activate.fish
+
+    
+    ## create a new virtualenv
+    else if [ $cmd = "mk" ]
+        if test -e $VENV_BASE/$VENV_NAME
+            echo "Virtualenv $VENV_NAME already exists. Can't create."
+            exit 1
+        else
+            # ask for the name of the new virtualenv
+            read -l VENV_NAME -c $VENV_NAME -p "set_color green; echo -n \"Create new virtualenv with name: \"; set_color normal; echo '> '"
+            virtualenv --prompt "($VENV_NAME) " $VENV_BASE/$VENV_NAME
+            source $VENV_BASE/$VENV_NAME/bin/activate.fish
+        end
+    
+    ## Activate a virtualenv
+    else if [ $cmd = "a" ]
+        if test -e $VENV_BASE/$VENV_NAME
+            source $VENV_BASE/$VENV_NAME/bin/activate.fish
+        else
+            echo "Virtualenv \"$VENV_NAME\" doesn't exists. Create it first by running \"venv mk $VENV_NAME\""
+        end
+
     else
-        virtualenv --prompt "($argv) " $VENV_BASE/$argv
-        source $VENV_BASE/$argv/bin/activate.fish
+        echo "Usage:"
+        echo "List virtualenvs:      venv ls"
+        echo "Create new virtualenv: venv mk [virtualenv_name] (default name of current dir)"
+        echo "Activate virtualenv:   venv a [virtualenv_name] (default name of current dir)"        
     end
 end
