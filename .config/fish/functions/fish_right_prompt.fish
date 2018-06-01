19ba32f4e0fa0f02002fb3d6fc9c4f8a0c8c5840
Daniel Klevebring <daniel.klevebring@gmail.com>
2018-06-01 21:04:37 +0200

    use green/red y/n instead of emojis

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,38 +1,42 @@
 function fish_right_prompt
     set -l __status $status
+    set -l items
+    set -l status_color
 
     if test $__status = 0
-        set items "😄 " "👍 " "😍 " "🖖 " "👊 " "👌 " "🙌 " "😎 "
-        #printf '[%s%s%s]' (set_color red) $__status (set_color normal)
+        #set items "😄 " "👍 " "😍 " "🖖 " "👊 " "👌 " "🙌 " "😎 "
+        set items "y " " y" "Y " " Y"
+        set status_color "green"
+        set status_message "succeeded"
     else
-        set items "😢 " "💥 " "👿 " "☠️ " "💩 " "🤢 "
+        #set items "😢 " "💥 " "👿 " "☠️ " "💩 " "🤢 "
+        set items "n " " n" "N " " N"
+        set status_color "red"
+        set status_message "failed"
     end
 
     set __item_index (python -c "import random, sys; print(random.randint(1, int(sys.argv[1])))" (count $items))
+
+    set_color $status_color    
     echo $items[$__item_index]
+    set_color normal
 
     if test $CMD_DURATION
         # Show duration of the last command
-        set duration (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}')
+        set -l duration (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}')
         echo " in $duration"
+        set -l cmd $history[1] 
 
         # OS X notification when a command takes longer than notify_duration
-        set notify_duration 10000
-        set exclude_cmd "bash|less|man|more|ssh|vim|nvim"
+        set -l notify_duration 10000
+        set -l exclude_cmd "bash|less|man|more|ssh|vim|nvim"
         if begin
                 test $CMD_DURATION -gt $notify_duration
-                and echo $history[1] | grep -vqE "^($exclude_cmd).*"
+                and echo $cmd | grep -vqE "^($exclude_cmd).*"
             end
-
-            # Only show the notification if iTerm or Terminal (active app name matches the string 'Term') is not focused
-            echo "
-                tell application \"System Events\"
-                    set activeApp to name of first application process whose frontmost is true
-                    if \"Term\" is not in activeApp then
-                        display notification \"Finished in $duration\" with title \"$history[1]\"
-                    end if
-                end tell
-                " | osascript
+            set -l notification_message "$cmd $status_message after $duration."
+            set -l title "Command $status_message"
+            send_system_notification $title $notification_message
         end
     end
 
