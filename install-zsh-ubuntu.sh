fb3dd773f381f9df1ea63820c1f4934e8ccc2eee
Daniel Klevebring <daniel.klevebring@gmail.com>
2020-02-26 08:13:00 +0100

    install missing plugins

--- install-zsh-ubuntu.sh
+++ install-zsh-ubuntu.sh
@@ -4,3 +4,6 @@ sudo apt install zsh
 sudo chsh -s /bin/zsh $USER
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
